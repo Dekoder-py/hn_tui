e7b4cffba91aaf952bb8789611b550b003146c1d
Dekoder-py <kyle@codingcorner.dev>
2026-05-22 10:13:44 +1200

    fetch hn

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
-use std::io::Error;
-
-use ratatui::{DefaultTerminal, Frame, crossterm};
+use ratatui::{
+    DefaultTerminal, Frame, crossterm,
+    layout::{Constraint, Layout},
+};
 use serde::Deserialize;
 
 #[derive(Deserialize)]
-struct HNData {
-    id: String,
-    url: String,
+struct Story {
     title: String,
+    url: Option<String>,
+    score: u32,
+    by: String,
 }
 
 fn main() -> color_eyre::Result<()> {
@@ -17,18 +19,50 @@ fn main() -> color_eyre::Result<()> {
 }
 
 fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
+    let stories: Vec<Story> = fetch_hn();
     loop {
-        terminal.draw(render)?;
+        terminal.draw(|frame| render(frame, &stories))?;
         if crossterm::event::read()?.is_key_press() {
             break Ok(());
         }
     }
 }
 
-fn render(frame: &mut Frame) {
-    frame.render_widget("Hello", frame.area());
+fn render(frame: &mut Frame, stories: &Vec<Story>) {
+    let layout = Layout::default()
+        .direction(ratatui::layout::Direction::Vertical)
+        .constraints(vec![Constraint::Percentage(40), Constraint::Percentage(50)])
+        .split(frame.area());
+
+    frame.render_widget(format!("{}", stories[0].title), layout[0]);
+    frame.render_widget(
+        format!("{}", stories[0].url.as_deref().unwrap_or("(no url)")),
+        layout[1],
+    );
 }
 
-fn fetch_hn() -> Result<String, Error> {
-    Ok("hi".to_string())
+fn fetch_hn() -> Vec<Story> {
+    let client = reqwest::blocking::Client::new();
+    let ids: Vec<u64> = client
+        .get("https://hacker-news.firebaseio.com/v0/topstories.json")
+        .send()
+        .expect("Failed to fetch from hacker news")
+        .json()
+        .expect("Failed to parse story ids");
+
+    let mut stories: Vec<Story> = vec![];
+
+    for (_, id) in ids.iter().take(20).enumerate() {
+        let url = format!("https://hacker-news.firebaseio.com/v0/item/{id}.json");
+        let story: Story = client
+            .get(&url)
+            .send()
+            .expect("Failed to fetch story")
+            .json()
+            .expect("Failed to parse story");
+
+        stories.push(story);
+    }
+
+    stories
 }
