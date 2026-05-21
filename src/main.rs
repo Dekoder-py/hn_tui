dae89fbe318f579b5d4bef6db5886d6635bfc152
Dekoder-py <kyle@codingcorner.dev>
2026-05-22 09:32:29 +1200

    create basic tui

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Error;
+
+use ratatui::{DefaultTerminal, Frame, crossterm};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct HNData {
+    id: String,
+    url: String,
+    title: String,
+}
+
+fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
+    ratatui::run(app)?;
+    Ok(())
+}
+
+fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
+    loop {
+        terminal.draw(render)?;
+        if crossterm::event::read()?.is_key_press() {
+            break Ok(());
+        }
+    }
+}
+
+fn render(frame: &mut Frame) {
+    frame.render_widget("Hello", frame.area());
+}
+
+fn fetch_hn() -> Result<String, Error> {
+    Ok("hi".to_string())
 }
